3ff36c3b4fdb30aff88c99403ba34838d8643254
Ángel Villalobos <angel.villalobos2596@gmail.com>
2019-10-10 23:13:01 -0600

    Update

--- src/Model/Casillero.java
+++ src/Model/Casillero.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package Model;
         
 public class Casillero {
     private boolean estado;
@@ -15,6 +15,7 @@ public class Casillero {
 
     public boolean asignarCasillero(Cliente pCliente){
 
+        return true;
     }
     
 }
--- src/Model/Counter.java
+++ src/Model/Counter.java
@@ -18,5 +18,6 @@ public class Counter {
     }
     public boolean registrarCliente(Cliente pCliente){
 
+        return true;
     }
 }
