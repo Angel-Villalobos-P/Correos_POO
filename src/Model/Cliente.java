8938e589f43ceb5a37f3270953a668849f18dc03
Zhong <liuguo35@hotmail.com>
2019-10-24 16:19:01 -0600

    Agregar,mod,elim,consult de Counter

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -41,5 +41,76 @@ public class Cliente {
                 ", Sexo=" + sexo + 
                 ", Paquetes Retirados=" + PaquetesRetirados + '}'+"\n";  
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public TipoRango getTipo() {
+        return tipo;
+    }
+
+    public TipoSexo getSexo() {
+        return sexo;
+    }
+
+    public int getPaquetesRetirados() {
+        return PaquetesRetirados;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setTipo(TipoRango tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setSexo(TipoSexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPaquetesRetirados(int paquetesRetirados) {
+        PaquetesRetirados = paquetesRetirados;
+    }
 }
